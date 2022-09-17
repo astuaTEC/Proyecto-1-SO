cc21bf14344330f6782ea344b91e78c918d7d6f9
Saymon Astúa Madrigal <42762900+astuaTEC@users.noreply.github.com>
2022-09-17 11:00:28 -0600

    Se comienzan las pruebas con semaforos

--- encoder.c
+++ encoder.c
@@ -1,4 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+// FOR FORK
+#include <sys/types.h>
+#include <unistd.h>
+
+// For killing child process
+#include <sys/wait.h>
+
+#define SHM_SEMS "sems_shared_memory"
 
 int main(){
 
