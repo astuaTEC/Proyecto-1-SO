cc9ec07fb9a5a228f608d4dc1863e51da8684278
JOhsua <josui77@hotmail.com>
2022-09-27 23:43:19 -0600

    First Interface Design

---
+++ GUI.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+
+
+static GtkWidget *square;
+static GtkWidget *square2;
+
+int main(int argc, char **argv) {
+
+    //Basic GTK widgets to design
+    GtkWidget *window, *grid, *calculate;
+
+    //Window size parameters
+    int W = 100;
+    int H = 100;
+
+    //Initialized the main window and set up 
+    //https://www.ubuntubuzz.com/2018/11/setup-cgtk-programming-tools-on-ubuntu-for-beginners.html
+    //https://github.com/engineer-man/youtube/blob/master/007/007_gtk.c
+    gtk_init(&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(window, "Image Generation");
+    gtk_window_set_default_size(window, W, H);
+    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    //Grind arrange interface elements in columns and rows
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
+
+    //gtk_label_new -- element to display the image pixel by pixel
+
+    square = gtk_label_new("");
+    gtk_grid_attach(GTK_GRID(grid), square, 0, 0, 1, 1);
+
+    square2 = gtk_label_new("");
+    gtk_grid_attach(GTK_GRID(grid), square2, 1, 0, 1, 1);
+
+    gtk_widget_show_all(window);
+    gtk_main();
+
+    return 0;
+}
